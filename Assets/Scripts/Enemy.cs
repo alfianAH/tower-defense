a59567a75179002456b85a60f870c418566ea578
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-04-27 00:22:36 +0800

    Make enemy's path

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -7,6 +8,9 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private SpriteRenderer healthBar,
         healthFill;
+    
+    public Vector3 TargetPosition { get; private set; }
+    public int CurrentPathIndex { get; private set; }
 
     private int currentHealth;
 
@@ -14,5 +18,51 @@ public class Enemy : MonoBehaviour
     {
         currentHealth = maxHealth;
         healthFill.size = healthBar.size;
+    }
+    
+    /// <summary>
+    /// Move to target position
+    /// </summary>
+    public void MoveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition, 
+            moveSpeed * Time.deltaTime);
+    }
+    
+    /// <summary>
+    /// Set enemy's target position
+    /// </summary>
+    /// <param name="targetPosition">Target Position</param>
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        TargetPosition = targetPosition;
+        healthBar.transform.parent = null;
+        
+        // Change enemy's rotation
+        Vector3 distance = TargetPosition - transform.position;
+        if (Mathf.Abs(distance.y) > Mathf.Abs(distance.x))
+        {
+            // If y axis of distance, ...
+            transform.rotation = Quaternion.Euler(distance.y > 0 
+                ? new Vector3(0f, 0f, 90f) // Rotate upward
+                : new Vector3(0f, 0f, -90f)); // Rotate downward
+        }
+        else
+        {
+            transform.rotation = quaternion.Euler(distance.x > 0 
+                ? new Vector3(0f, 0f, 0f) // Rotate to the right (default)
+                : new Vector3(0f, 0f, 180f)); // Rotate to the left
+        }
+
+        healthBar.transform.parent = transform;
+    }
+    
+    /// <summary>
+    /// Set current path index
+    /// </summary>
+    /// <param name="currentIndex"></param>
+    public void SetCurrentPathIndex(int currentIndex)
+    {
+        CurrentPathIndex = currentIndex;
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelManager : MonoBehaviour
 {
@@ -23,12 +24,60 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject towerUiPrefab;
 
     [SerializeField] private Tower[] towerPrefabs;
+    [SerializeField] private Enemy[] enemyPrefabs;
+
+    [SerializeField] private Transform[] enemyPaths;
+    [SerializeField] private float spawnDelay = 5f;
 
     private List<Tower> spawnedTowers = new List<Tower>();
+    private List<Enemy> spawnedEnemies = new List<Enemy>();
 
+    private float runningSpawnDelay;
+    
     private void Start()
     {
         InstantiateAllTowerUi();
+    }
+
+    private void Update()
+    {
+        // Counter is to spawn the enemy in certain delay
+        // Time.unscaledDeltaTime is independent delta time, unaffected by anything but itself,
+        // so it can be used as a timer
+        runningSpawnDelay -= Time.unscaledDeltaTime;
+
+        if (runningSpawnDelay <= 0f)
+        {
+            SpawnEnemy();
+            runningSpawnDelay = spawnDelay;
+        }
+
+        foreach (Enemy enemy in spawnedEnemies)
+        {
+            if(!enemy.gameObject.activeSelf) continue;
+            
+            // If the difference between enemy's position and target position is less than 0.1
+            if (Vector2.Distance(enemy.transform.position, enemy.TargetPosition) < 0.1f)
+            {
+                enemy.SetCurrentPathIndex(enemy.CurrentPathIndex + 1);
+                
+                // If enemy's current path index less than total of enemy paths, ...
+                if (enemy.CurrentPathIndex < enemyPaths.Length)
+                {
+                    // Set new target position
+                    enemy.SetTargetPosition(enemyPaths[enemy.CurrentPathIndex].position);
+                }
+                else // If enemy reaches the last path, ...
+                {
+                    // Deactivate the object
+                    enemy.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                enemy.MoveToTarget();
+            }
+        }
     }
     
     /// <summary>
@@ -54,5 +103,49 @@ public class LevelManager : MonoBehaviour
     public void RegisterSpawnedTowers(Tower tower)
     {
         spawnedTowers.Add(tower);
+    }
+    
+    /// <summary>
+    /// Spawn the enemy
+    /// </summary>
+    public void SpawnEnemy()
+    {
+        // Choose random enemy
+        int randomIndex = Random.Range(0, enemyPrefabs.Length);
+        string enemyIndexString = (randomIndex + 1).ToString();
+
+        // Check enemy on hierarchy
+        // Return object enemy in game object is not active and contains index in its name
+        // Else null
+        GameObject newEnemyObj = spawnedEnemies.Find(
+                e => !e.gameObject.activeSelf && e.name.Contains(enemyIndexString))
+            ?.gameObject;
+        
+        // If newEnemyObj is null, then make new one
+        if (newEnemyObj == null)
+        {
+            newEnemyObj = Instantiate(enemyPrefabs[randomIndex].gameObject);
+        }
+        
+        Enemy newEnemy = newEnemyObj.GetComponent<Enemy>();
+        
+        if (!spawnedEnemies.Contains(newEnemy))
+        {
+            spawnedEnemies.Add(newEnemy);
+        }
+        
+        newEnemy.transform.position = enemyPaths[0].position;
+        newEnemy.SetTargetPosition(enemyPaths[1].position);
+        newEnemy.SetCurrentPathIndex(1);
+        newEnemy.gameObject.SetActive(true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < enemyPaths.Length - 1; i++)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(enemyPaths[i].position, enemyPaths[i + 1].position);
+        }
     }
 }
