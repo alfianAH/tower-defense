ce89a396628fd0b379b02887057955755bc63e53
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-04-26 23:36:36 +0800

    Make enemy prefabs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 1;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private SpriteRenderer healthBar,
+        healthFill;
+
+    private int currentHealth;
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+        healthFill.size = healthBar.size;
+    }
+}
